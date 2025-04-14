a347dc3d0237f86d54c365472b1aacf477930bd8
unknown <yatte.acho@uvci.edu.ci>
2025-04-14 22:06:11 +0000

    Etat Civil version 1

--- src/main/java/com/saasdemo/backend/config/SwaggerConfig.java
+++ src/main/java/com/saasdemo/backend/config/SwaggerConfig.java
@@ -4,12 +4,25 @@ import org.springframework.context.annotation.Configuration;
 
 import com.saasdemo.backend.dto.Bean;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 
-//configuration de swagger 
+//configuration de swagger avec JWT 
 @Configuration
+@OpenAPIDefinition(
+   security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
 public class SwaggerConfig {
 
   @Bean
